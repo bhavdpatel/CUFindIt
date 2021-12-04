37774f001ab3e41b6e1046fe345e7cd8f144f4f1
Sunrise <84783571+Sunrise614@users.noreply.github.com>
2021-12-03 21:45:03 -0500

    Update databases.py

--- src/databases.py
+++ src/databases.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 import datetime
-import bcrypt
 import hashlib
 
 db = SQLAlchemy()
@@ -12,8 +11,8 @@ class Item(db.Model):
     name = db.Column(db.String, nullable=False)
     image_name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
-    id_found = db.relationship("user_found", back_populates="found_items", nullable=False)
-    id_claimed = db.relationship("user_claimed", back_populates="claimed_items", nullable=True)
+    id_found = db.relationship("user_found", back_populates="found_items")
+    id_claimed = db.relationship("user_claimed", back_populates="claimed_items")
     date_found = db.Column(db.String, nullable=False)
     date_claimed = db.Column(db.String, nullable=True)
 
@@ -49,7 +48,6 @@ class User(db.Model):
     claimed_items = db.relationship("claimed_items", back_populates="item")
     found_items = db.relationship("found_items", back_populates="item")
 
-    #Session information
     session_token = db.Column(db.String, nullable=False, unique=True)
     session_expiration = db.Column(db.DateTime, nullable=False)
     update_token = db.Column(db.String, nullable=False, unique=True)
@@ -60,22 +58,9 @@ class User(db.Model):
             password.encode("utf8"), bcrypt.gensalt(rounds=13))
         self.renew_session()
 
-    # Used to randomly generate session/update tokens
     def _urlsafe_base_64(self):
         return hashlib.sha1(os.urandom(64)).hexdigest()
 
-    # Generates new tokens, and resets expiration time
-    def renew_session(self):
-        self.session_token = self._urlsafe_base_64()
-        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=7)
-        self.update_token = self._urlsafe_base_64()
-
-    def verify_password(self, password):
-        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
-
-    # Checks if session token is valid and hasn't expired
-    def verify_session_token(self, session_token):
-        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
 
     def serialize(self):
         return {
@@ -89,9 +74,5 @@ class User(db.Model):
         return {
             "id": self.id,
             "netid": self.netid,
-            "email": self.email,
-            # "lost": [l.serialize() for l in self.lost],
-            # "found": [f.serialize() for f in self.found]
-        
+            "email": self.email
         }
-
